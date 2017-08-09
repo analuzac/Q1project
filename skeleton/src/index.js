11761d3a44a3ab44b2bf4574be17d94efab7d96c
Ana Luz Acevedo-Cabrera <analuzacevedo-cabrera@Anas-MacBook.local>
2017-08-09 09:17:23 -0700

    working widget, now refactoring to make sure everything is in elementx

--- skeleton/src/index.js
+++ skeleton/src/index.js
@@ -1,54 +1,86 @@
+
+const { div, img, h5, ul, li } = require('elementx')
+const CraigslistPageScraper = require('./scrapers/CraigslistPageScraper');
+
 addEventListener('DOMContentLoaded', main);
 
-function main() {
+
+function main(){
+let input = document.getElementById("url");
+document.getElementById("submit-button").addEventListener("click", () =>{
+const scraper = new CraigslistPageScraper();
+  scraper.scrape(input.value).then(data => {
+    buildWidget(data);
+    //console.log(data);
+  });
+});
+}
+
+
+
+function buildWidget(widgetData) {
+
   const $root = document.querySelector('#root');
-  const $app = null; // <== Your dynamically generated DOM element goes here
+  const $app = div(
+    div({ class: 'row' },
+      div({ class: 'col s8 m4' },
+        div({ class: 'card white' },
+          div({ class: 'card-image' }),
+          img({ src: widgetData.image,
+              height: '100%',
+              width: '100%' }),
+          div({ class: 'card-content black-text' },
+            h5({class:'black-text center'},widgetData.price),
+            ul(
+              li({class:'center'},widgetData.location),
+              li({class:'center'},widgetData.timeline)
+            )
+          )
+        )
+      )
+    )
+  );
+
   $root.appendChild($app);
+
 }
 
 
 
 
 
-//Dynamic Menu Section
-let menuData=[{food:'Royale with Cheese', price:8.99, image:'img/burger.jpg', id:'burger'},
-{food:'Argula Pie', price:11.99, image:'img/pizza.jpg', id:'pizza'},
-{food:'Smoked Swine', price:14.99, image:'img/ribs.jpg', id:'ribs'},
-{food:'Ice Cream Biscut', price:7.99, image:'img/ice_cream.jpg', id:'icecream'}
-];
+// //Dynamic Section - old school way before I learned about elementx
+//
+// document.getElementById("submit-button").addEventListener("click", function () {
 
-let menuDiv=document.getElementById('menu');
-
-for (let i=0;i<menuData.length;i++){
-
-
-  let colDiv=document.createElement('div');
-  colDiv.setAttribute('class', 'col s12 m6');
-  let cardColorDiv=document.createElement('div');
-  cardColorDiv.setAttribute('class', 'card white');
+  // let colDiv = document.createElement('div');
+  // colDiv.setAttribute('class', 'col s6 m6 l6');
+  // let cardColorDiv = document.createElement('div');
+  // cardColorDiv.setAttribute('class', 'card white');
 
   //image section
-  let cardImageDiv=document.createElement('div');
-  cardImageDiv.setAttribute('class', 'card-image');
-  let imageDiv=document.createElement('img');
-  imageDiv.setAttribute('src', menuData[i].image);
-  cardImageDiv.appendChild(imageDiv);
-  cardColorDiv.appendChild(cardImageDiv);
+  // let cardImageDiv = document.createElement('div');
+  // cardImageDiv.setAttribute('class', 'card-image');
+  // let imageDiv = document.createElement('img');
+  // imageDiv.setAttribute('src', widgetData.image);
+  // cardImageDiv.appendChild(imageDiv);
+  // cardColorDiv.appendChild(cardImageDiv);
 
-  //info section
-  let cardContentDiv=document.createElement('div');
-  cardContentDiv.setAttribute('class', 'card-content black-text');
-  let h5Div=document.createElement('h5');
-  h5Div.setAttribute('class', 'black-text');
-  h5Div.innerText=menuData[i].food;
-  let pDiv=document.createElement('p');
-  pDiv.innerText=`$${menuData[i].price}`;
-  cardContentDiv.appendChild(h5Div);
-  cardContentDiv.appendChild(pDiv);
-  cardColorDiv.appendChild(cardContentDiv);
+  // //info section
+  // let cardContentDiv = document.createElement('div');
+  // cardContentDiv.setAttribute('class', 'card-content black-text');
+  // let h5Div = document.createElement('h5');
+  // h5Div.setAttribute('class', 'black-text');
+  // h5Div.innerText = widgetData.price;
+  // let pDiv = document.createElement('p');
+  // pDiv.innerText = widgetData.location;
+  // pDiv.innerText = widgetData.timeline;
+  // cardContentDiv.appendChild(h5Div);
+  // cardContentDiv.appendChild(pDiv);
+  // cardContentDiv.appendChild(pDiv);
+  // cardColorDiv.appendChild(cardContentDiv);
 
+  // colDiv.appendChild(cardColorDiv);
+  // rootDiv.appendChild(colDiv);
 
-  colDiv.appendChild(cardColorDiv);
-  menuDiv.appendChild(colDiv);
-
-}
+// });
--- skeleton/src/scrapers/CraigslistPageScraper.js
+++ skeleton/src/scrapers/CraigslistPageScraper.js
@@ -1,14 +1,11 @@
-const url='https://sfbay.craigslist.org/sfc/roo/d/great-room-available-in/6247349633.html';
+//const url='https://sfbay.craigslist.org/sfc/roo/d/great-room-available-in/6247349633.html';
 
 
 class CraigslistPageScraper {
-  constructor(url) {
-    this.url = url;
-  }
-
   scrape(url){
     //const url='https://sfbay.craigslist.org/sfc/roo/d/great-room-available-in/6247349633.html';
-    fetch(`http://cors-bypass-proxy.axiomlogic.com/${url}`).then(response => {
+    //TODo - think about how to return a promise from this method (scrape)
+    return fetch(`http://cors-bypass-proxy.axiomlogic.com/${url}`).then(response => {
       return response.text();
     })
     .then(html => {
@@ -16,24 +13,33 @@ class CraigslistPageScraper {
       let parser = new DOMParser();
       let doc = parser.parseFromString(html, "text/html");
       //console.log(">>>>>>>",doc);
+
       let price = doc.getElementsByClassName("price")[0].innerText;
       let location = doc.getElementsByTagName("small")[0].innerText;
       let timeline = doc.getElementsByClassName("housing_movein_now property_date shared-line-bubble")[0].innerText;
       let attrs = doc.getElementsByClassName("mapAndAttrs")[0].children[2].children;
       //console.log(attrs);
-      let info = [];
-      for (i=0;i<attrs.length;i++){
+      let details = [];
+      for (let i=0;i<attrs.length;i++){
         if (i%2===0){
-          info.push(attrs[i].innerText);
+          details.push(attrs[i].innerText);
         }
       }
       let image = doc.getElementsByClassName("thumb")[0].href;
       //console.log(image);
 
-      return doc;
+      let widgetData={};
+      widgetData.image = image;
+      widgetData.price = price;
+      widgetData.location = location;
+      widgetData.timeline = timeline;
+
+      return widgetData;
     })
 
 
-  })
+  }
 
 }
+
+module.exports = CraigslistPageScraper;
